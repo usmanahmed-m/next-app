0ca3b4870831d7baa5d15b8e8552ca9ebb5c9ec3
Usman Ahmed <69243669+usmanahmed-m@users.noreply.github.com>
2024-02-09 04:46:22 +0500

    cache behavior

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -6,7 +6,11 @@ type User = {
 };
 
 const UsersPage = async () => {
-  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const res = await fetch('https://jsonplaceholder.typicode.com/users', {
+    // Cache behavior will only work in fetch fn. If using axios it won't work
+    // cache: 'no-store', // next by default cache data. To negate this behavior we pass 2nd arg to fetch as object
+    // next: { revalidate: 10 }, // with next revalidate, next will update data after 10s if we have data which changes frequently.
+  });
   const users: User[] = await res.json();
   return (
     <>
