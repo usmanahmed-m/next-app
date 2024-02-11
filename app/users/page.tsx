b6ddcf53df9280f4aeadf4d155dc7144c2d01a7f
Usman Ahmed <69243669+usmanahmed-m@users.noreply.github.com>
2024-02-11 17:31:16 +0500

    css module

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -5,6 +5,12 @@ type User = {
   name: string;
 };
 
+// By default cache behavior is on.
+// Two types of rendering. Client side and Server side
+// client side rendering is same as in react
+// Server side has 2 types of rendering: 1- static rendering and dynamic rendering
+// static rendering: Next will cache all data in build time and serve when requested. In this way way don't get real time data
+// dynamic rendering: Next will not cache data and while request time it will create page and serve with latest data. To implement this behavior pass {cache: 'no-store' }
 const UsersPage = async () => {
   const res = await fetch('https://jsonplaceholder.typicode.com/users', {
     // Cache behavior will only work in fetch fn. If using axios it won't work
