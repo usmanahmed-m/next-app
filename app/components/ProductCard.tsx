c436fdc07ea9965ae9a71c734955aa7b9956a026
Usman Ahmed <69243669+usmanahmed-m@users.noreply.github.com>
2024-02-11 18:03:40 +0500

    adding tailwind utility classes

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import AddToCart from './AddToCart';
-import styles from './ProductCard.module.css';
+// import styles from './ProductCard.module.css';
 
+// Benefits of using tailwind
+// With tailwind when we build our application, our final css bundle will only have the utility classes we have used in our markup
 const ProductCard = () => {
   return (
-    <div className={styles.card}>
+    // <div className={styles.card}>
+    <div className='p-5 my-5 bg-sky-400 text-white text-xl hover:bg-sky-500'>
       <AddToCart />
     </div>
   );
